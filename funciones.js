093920f7d8d09faf65aa1d65fbcad430cb387b56
CarlosValleRamirez <Carlos_Valle_Ramirez@hotmail.com>
2020-11-11 13:13:12 -0600

    Actividadi10

---
+++ funciones.js
@@ -0,0 +1,14 @@
+//Variables
+
+const Numero = 7;
+const Texto = "Hola, Soy Carlos y soy alcoholico";
+const booleana = false;
+
+let sinValor
+
+//Tipos de datos
+
+console.log(typeof Numero);
+consoloe.log(typeof Texto);
+console.log(typeof booleana);
+console.log(typeof sinValor);
